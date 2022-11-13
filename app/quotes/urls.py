350401b0e8ef48af09417ec0097fb8cea59b0348
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-13 10:54:43 +0900

    search result by Q

--- app/quotes/urls.py
+++ app/quotes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.QuoteList.as_view(), name='quote')
+        path('', views.QuoteList.as_view(), name='quote'),
+        path('searchresults/', views.SearchResultsList.as_view(), name='searchresults')
         ]
--- app/quotes/views.py
+++ app/quotes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+#from django.contrib.postgres.search import 
+from django.db.models import Q
+
 
 from .models import Quote
 
@@ -9,3 +12,13 @@ class QuoteList(ListView):
     context_object_name = 'quotes'
 
     
+class SearchResultsList(ListView):
+    model = Quote
+    template_name = 'search.html'
+    context_object_name = 'quotes'
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Quote.objects.filter(
+            Q(name__icontains=query) | Q(quote__icontains=query)
+        )
